4150d818c5eb2b9ed17c1a8cab610a9edba86e77
Divya Singh <divyasingh0612@gmail.com>
2025-06-13 01:10:17 -0700

    Added Schema for Google

--- models/user.js
+++ models/user.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  phone: { type: String, required: true, unique: true },
+ phone: {
+  type: String,
+  required: function () {
+    return !this.googleId; // agar googleId nahi hai, matlab OTP login ho raha
+  },
+  unique: true,
+},
+
+  // phone: { type: String, required: true, unique: true },
   otp: String,
   otpExpiresAt: Date,
   email: { type: String, unique: true, sparse: true },
